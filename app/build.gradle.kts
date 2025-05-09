cdd031572901369c5e117c5a306531e4c0bdf29a
micorleone21 <2022101027@ms.bulsu.edu.ph>
2025-05-09 16:32:33 +0800

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.pokemonapp"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
